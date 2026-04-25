1a999860df860b9027d461b02ba9376f9cb36309
awsl1414 <3030994569@qq.com>
2026-04-25 19:45:05 +0800

    Initial Commit

---
+++ InputLock/InputLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputLockApp.swift
+//  InputLock
+//
+//  Created by logan on 2026/4/25.
+//
+
+import SwiftUI
+
+@main
+struct InputLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
